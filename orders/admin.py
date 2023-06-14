df9db2750c7c35874027c0ccb78b6f7882547745
FernandoAragonMoreno <vehiculos.aragon@gmail.com>
2023-06-14 15:36:57 -0500

    Adición función se incluye finción para traer el nombre del objeto, se importa User

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Country, City, City_Customer, Customer, PaymentMethods, PaymentMethods_Customer, Role, Seller #Ejemplo
+from .models import Country, City, City_Customer, Customer, PaymentMethods, PaymentMethods_Customer, Role, Seller #Ejemplo, 
 
 # Register your models here.
 #admin.site.register(Ejemplo)
@@ -11,3 +11,4 @@ admin.site.register(PaymentMethods)
 admin.site.register(PaymentMethods_Customer)
 admin.site.register(Role)
 admin.site.register(Seller)
+#admin.site.register(User)
--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # class Ejemplo(models.Model):
 #     idEjemplo = models.IntegerField()
 #     name = models.CharField(max_length=50)
 #     description = models.CharField(max_length=200)
+
 
 class Country(models.Model):
     idCountry = models.IntegerField()
@@ -13,6 +15,7 @@ class Country(models.Model):
     assetCountry = models.BooleanField()
 
 class City(models.Model):
+    idCountry = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
     idCity = models.IntegerField()
     nameCity = models.CharField(max_length=50)
     descriptionCity = models.CharField(max_length=200)
@@ -20,11 +23,15 @@ class City(models.Model):
     photo = models.ImageField(upload_to="photo")
     assetCity = models.BooleanField()
 
+    def __str__(self):
+        return self.nameCity
+
 class City_Customer(models.Model):
     idCity = models.IntegerField()
     idCustomer = models.IntegerField()
 
 class Customer(models.Model):
+    idUser = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
     idCustomer = models.IntegerField()
     nameCustomer = models.CharField(max_length=50)
     assetCustomer = models.BooleanField()
@@ -39,11 +46,13 @@ class PaymentMethods_Customer(models.Model):
     idCustomer = models.IntegerField()
 
 class Role(models.Model):
+    idUser = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
     idRole = models.IntegerField()
     nameRole = models.CharField(max_length=50)
     assetRole = models.BooleanField()
 
 class Seller(models.Model):
+    idUser = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
     idSeller = models.IntegerField()
     nameSeller = models.CharField(max_length=50)
     assetSeller = models.BooleanField()
